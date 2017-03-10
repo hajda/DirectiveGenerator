0c14b2ac89d522da6e6548410b74b89c038ad94e
David Hajba <david.hajba@ericsson.com>
2017-03-10 09:40:57 +0100

    [noartf] - stuff

--- src/main/java/directivegenerator/DirectiveGenerator.java
+++ src/main/java/directivegenerator/DirectiveGenerator.java
@@ -9,36 +9,48 @@ import java.io.UnsupportedEncodingException;
  * Created by PD on 2016.05.08..
  */
 public class DirectiveGenerator {
-    public void generateDirective(final String moduleName, final String prefix, final String directiveName, final boolean state) {
+    public void generateDirective(final String myModule, final String pfx, final String myDirective, final boolean state) {
 
         // TODO handle bad directive name
 
-        final String bareDirectiveName = directiveName.substring(0, 1).toUpperCase() + directiveName.substring(1);
-        final String controllerPrefix = prefix.substring(0, 1).toUpperCase() + prefix.substring(1);
-        final String controllerName = controllerPrefix + bareDirectiveName + "Controller";
+        /** pfxMyDirective */
+        final String[] splitDirectiveName = splitDirectiveName(myDirective);
+        capitalize(splitDirectiveName);
+        final String MyDirective = String.join("", splitDirectiveName);
+        final String pfxMyDirective = pfx + MyDirective; // pfxMyDirective
 
-        final File scriptsFolder = new File("scripts/components/" + directiveName + "/");
+        /** pfx-my-directive */
+        final String pfx_my_directive = normalizeDirectiveName(pfxMyDirective);
+
+
+
+        //        final String MyDirective = myDirective.substring(0, 1).toUpperCase() + myDirective.substring(1);
+        final String Pfx = pfx.substring(0, 1).toUpperCase() + pfx.substring(1);
+        final String MyDirectiveController = Pfx + MyDirective + "Controller";
+        final String MyDirectiveFilter = Pfx + MyDirective + "Filter";
+
+        final File scriptsFolder = new File("scripts/components/" + myDirective + "/");
         scriptsFolder.mkdirs();
-        final File appFolder = new File("scripts/app/" + directiveName + "/");
+        final File appFolder = new File("scripts/app/" + myDirective + "/");
         appFolder.mkdirs();
 
         PrintWriter writer;
         try {
             /* directive */
 
-            writer = new PrintWriter("scripts/components/" + directiveName + "/" + directiveName + ".directive.js", "UTF-8");
-            writer.println("(function " + directiveName + "DirectiveDefinition() {");
+            writer = new PrintWriter("scripts/components/" + myDirective + "/" + myDirective + ".directive.js", "UTF-8");
+            writer.println("(function " + myDirective + "DirectiveDefinition() {");
             writer.println("    'use strict';");
-            writer.println("    angular.module('" + moduleName + "')");
-            writer.println("        .directive('" + prefix + bareDirectiveName + "', " + directiveName + "Directive);");
+            writer.println("    angular.module('" + myModule + "')");
+            writer.println("        .directive('" + pfx + MyDirective + "', " + myDirective + "Directive);");
             writer.println("");
-            writer.println("    " + directiveName + "Directive.$inject = [];");
+            writer.println("    " + myDirective + "Directive.$inject = [];");
             writer.println("");
-            writer.println("    function " + directiveName + "Directive() {");
+            writer.println("    function " + myDirective + "Directive() {");
             writer.println("        return {");
             writer.println("            restrict: 'AE',");
-            writer.println("            templateUrl: 'scripts/components/" + directiveName + "/" + directiveName + ".template.html',");
-            writer.println("            controller: '" + controllerName + "',");
+            writer.println("            templateUrl: 'scripts/components/" + myDirective + "/" + myDirective + ".template.html',");
+            writer.println("            controller: '" + MyDirectiveController + "',");
             writer.println("            controllerAs: 'vm',");
             writer.println("            link: function postLink($scope, elementInstance, attributeInstances, controller) {");
             writer.println("");
@@ -51,16 +63,16 @@ public class DirectiveGenerator {
 
             /* controller */
 
-            writer = new PrintWriter("scripts/components/" + directiveName + "/" + directiveName + ".controller.js", "UTF-8");
-            writer.println("(function " + directiveName + "ControllerDefinition() {");
+            writer = new PrintWriter("scripts/components/" + myDirective + "/" + myDirective + ".controller.js", "UTF-8");
+            writer.println("(function " + myDirective + "ControllerDefinition() {");
             writer.println("    'use strict';");
             writer.println("");
-            writer.println("    angular.module('" + moduleName + "')");
-            writer.println("        .controller('" + controllerName + "', " + directiveName + "Controller);");
+            writer.println("    angular.module('" + myModule + "')");
+            writer.println("        .controller('" + MyDirectiveController + "', " + myDirective + "Controller);");
             writer.println("");
-            writer.println("    " + directiveName + "Controller.$inject = [];");
+            writer.println("    " + myDirective + "Controller.$inject = [];");
             writer.println("");
-            writer.println("    function " + directiveName + "Controller() {");
+            writer.println("    function " + myDirective + "Controller() {");
             writer.println("        var vm = this;");
             writer.println("    }");
             writer.println("})();");
@@ -69,16 +81,16 @@ public class DirectiveGenerator {
 
             /* Service (factory method) */
 
-            writer = new PrintWriter("scripts/components/" + directiveName + "/" + directiveName + ".service.js", "UTF-8");
-            writer.println("(function " + directiveName + "ServiceDefinition() {");
+            writer = new PrintWriter("scripts/components/" + myDirective + "/" + myDirective + ".service.js", "UTF-8");
+            writer.println("(function " + myDirective + "ServiceDefinition() {");
             writer.println("    'use strict';");
             writer.println("");
-            writer.println("    angular.module('" + moduleName + "')");
-            writer.println("        .factory('" + controllerPrefix + bareDirectiveName + "', " + directiveName + "Service);");
+            writer.println("    angular.module('" + myModule + "')");
+            writer.println("        .factory('" + Pfx + MyDirective + "', " + myDirective + "Service);");
             writer.println("");
-            writer.println("    " + directiveName + "Service.$inject = [];");
+            writer.println("    " + myDirective + "Service.$inject = [];");
             writer.println("");
-            writer.println("    function " + directiveName + "Service() {");
+            writer.println("    function " + myDirective + "Service() {");
             writer.println("        return {");
             writer.println("        };");
             writer.println("    }");
@@ -88,17 +100,17 @@ public class DirectiveGenerator {
 
             /* Filter */
 
-            writer = new PrintWriter("scripts/components/" + directiveName + "/" + directiveName + ".filter.js", "UTF-8");
-            writer.println("(function " + directiveName + "FilterDefinition() {");
+            writer = new PrintWriter("scripts/components/" + myDirective + "/" + myDirective + ".filter.js", "UTF-8");
+            writer.println("(function " + myDirective + "FilterDefinition() {");
             writer.println("    'use strict';");
             writer.println("");
-            writer.println("    angular.module('" + moduleName + "')");
-            writer.println("        .filter('" + controllerPrefix + bareDirectiveName + "', " + directiveName + "Filter);");
+            writer.println("    angular.module('" + myModule + "')");
+            writer.println("        .filter('" + Pfx + MyDirective + "Filter', " + myDirective + "Filter);");
             writer.println("");
-            writer.println("    " + directiveName + "Filter.$inject = [];");
+            writer.println("    " + myDirective + "Filter.$inject = [];");
             writer.println("");
-            writer.println("    function " + directiveName + "Filter() {");
-            writer.println("        return function " + directiveName + "(input, arg1, arg2) {");
+            writer.println("    function " + myDirective + "Filter() {");
+            writer.println("        return function " + myDirective + "(input, arg1, arg2) {");
             writer.println("            return input && (arg1 || arg2);");
             writer.println("        };");
             writer.println("    }");
@@ -108,31 +120,36 @@ public class DirectiveGenerator {
 
             /* template */
 
-            writer = new PrintWriter("scripts/components/" + directiveName + "/" + directiveName + ".template.html", "UTF-8");
-            writer.println("TODO generate state");
+            writer = new PrintWriter("scripts/components/" + myDirective + "/" + myDirective + ".template.html", "UTF-8");
+            writer.println("<!-- TODO generate template -->");
             writer.close();
 
 
             /* State and state template */
 
-            writer = new PrintWriter("scripts/app/" + directiveName + "/" + directiveName + ".js", "UTF-8");
+            String readableName = MyDirective;
+            splitDirectiveName(readableName);
+            readableName = String.join(" ", readableName);
 
-            writer.println("(function " + directiveName + "StateDefinition() {");
+            writer = new PrintWriter("scripts/app/" + myDirective + "/" + myDirective + ".js", "UTF-8");
+
+            writer.println("(function " + myDirective + "StateDefinition() {");
             writer.println("    'use strict';");
-            writer.println("    angular.module('" + moduleName + "')");
+            writer.println("    angular.module('" + myModule + "')");
             writer.println("        .config(function ($stateProvider) {");
             writer.println("            $stateProvider");
-            writer.println("                .state('" + directiveName + "', {");
+            writer.println("                .state('" + myDirective + "', {");
             writer.println("                    parent: 'site', // TODO review parent");
-            writer.println("                    url: '/" + directiveName + "', // TODO review URL");
+            writer.println("                    url: '/" + pfx_my_directive + "', // TODO review URL");
             writer.println("                    data: {");
             writer.println("                        authorities: [], // TODO set up access rights");
-            writer.println("                        pageTitle: '" + directiveName + "' // TODO review page title");
+            writer.println("                        pageTitle: '" + myDirective + "' // TODO review page title");
+            writer.println("                        readableName: '" + readableName + "' // TODO review page title");
             writer.println("                },");
             writer.println("                views: {");
             writer.println("                    'content@': {");
-            writer.println("                        templateUrl: 'scripts/app/" + directiveName + "/" + directiveName + ".html'//,   // TODO update template url if the state is not directly under the app directory");
-            writer.println("                        // controller: '" + controllerName + "' // TODO review hooked controller");
+            writer.println("                        templateUrl: 'scripts/app/" + myDirective + "/" + myDirective + ".html'//,   // TODO update template url if the state is not directly under the app directory");
+            writer.println("                        // controller: '" + MyDirectiveController + "' // TODO review hooked controller");
             writer.println("                    }");
             writer.println("                }");
             writer.println("            });");
@@ -141,21 +158,21 @@ public class DirectiveGenerator {
 
             writer.close();
 
-            writer = new PrintWriter("scripts/app/" + directiveName + "/" + directiveName + ".html", "UTF-8");
-            writer.println("<!-- TODO invoke generated directive -->");
+            writer = new PrintWriter("scripts/app/" + myDirective + "/" + myDirective + ".html", "UTF-8");
+            writer.println("<" + pfx_my_directive + "></" + pfx_my_directive + ">");
             writer.close();
 
 
             /* index references */
 
-            writer = new PrintWriter(directiveName + "-index.html", "UTF-8");
-            writer.println("    <!-- " + directiveName + " directive -->");
-            writer.println("    <script src=\"scripts/app/" + directiveName + "/" + directiveName + ".js\"></script>");
-            writer.println("    <script src=\"scripts/components/" + directiveName + "/" + directiveName + ".service.js\"></script>");
-            writer.println("    <script src=\"scripts/components/" + directiveName + "/" + directiveName + ".controller.js\"></script>");
-            writer.println("    <script src=\"scripts/components/" + directiveName + "/" + directiveName + ".directive.js\"></script>");
-            writer.println("    <script src=\"scripts/components/" + directiveName + "/" + directiveName + ".filter.js\"></script>");
-            writer.println("    <!-- end " + directiveName + " directive -->");
+            writer = new PrintWriter(myDirective + "-index.html", "UTF-8");
+            writer.println("    <!-- " + myDirective + " directive -->");
+            writer.println("    <script src=\"scripts/app/" + myDirective + "/" + myDirective + ".js\"></script>");
+            writer.println("    <script src=\"scripts/components/" + myDirective + "/" + myDirective + ".service.js\"></script>");
+            writer.println("    <script src=\"scripts/components/" + myDirective + "/" + myDirective + ".controller.js\"></script>");
+            writer.println("    <script src=\"scripts/components/" + myDirective + "/" + myDirective + ".directive.js\"></script>");
+            writer.println("    <script src=\"scripts/components/" + myDirective + "/" + myDirective + ".filter.js\"></script>");
+            writer.println("    <!-- end " + myDirective + " directive -->");
             writer.close();
 
         } catch (final FileNotFoundException e) {
@@ -163,5 +180,37 @@ public class DirectiveGenerator {
         } catch (final UnsupportedEncodingException e) {
             e.printStackTrace();
         }
+    }
+
+
+    private String denormalizeDirectiveName(final String directiveName) {
+        final String[] splitDirectiveName = directiveName.split("-");
+        capitalize(splitDirectiveName);
+        return String.join("", splitDirectiveName);
+    }
+
+    private String normalizeDirectiveName(final String directiveName) {
+        final String[] splitDirectiveName = splitDirectiveName(directiveName);
+        toLowerCase(splitDirectiveName);
+        return String.join("-", splitDirectiveName);
+    }
+
+    private String[] splitDirectiveName(final String directiveName) {
+        return directiveName.split("(?=\\p{Upper})");
+    }
+
+    private void toLowerCase(final String...strings) {
+        for (final String string : strings) {
+            string.toLowerCase();
+        }
+    }
+
+    private void capitalize(final String...strings) {
+        for (String string : strings) {
+            final char first = Character.toUpperCase(string.charAt(0));
+            string = first + string.substring(1);
+            System.out.print(string);
+        }
+        System.out.println();
     }
 }
